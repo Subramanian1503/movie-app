9235273b0fbdce319e47f1d51dc1c69b79f703a9
Subramanian1503 <roopan1503@gmail.com>
2023-11-01 22:21:09 +0530

    Added UI for home page

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,29 @@
+import { data } from "../data";
+import Navbar from "./Navbar";
+import MovieCard from "./MovieCard";
+
 function App() {
-  return <div className="App">project setup</div>;
+  return (
+    <div className="App">
+      {/* nav bar component */}
+      <Navbar />
+      {/* main container */}
+      <div className="main">
+        <div className="tabs">
+          <div className="tab">Movies</div>
+          <div className="tab">Favoutites</div>
+        </div>
+
+        <div className="list">
+          {data.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      </div>
+      {/* tabs */}
+      {/* movie cards */}
+    </div>
+  );
 }
 
 export default App;
