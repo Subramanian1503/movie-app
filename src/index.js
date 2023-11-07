048b70815d8007ca8e67d9e783d84328f1c4e7c3
Subramanian1503 <roopan1503@gmail.com>
2023-11-07 19:30:47 +0530

    Added middleware from redux to log actions getting disptached

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 
 import "./index.css";
 import App from "./components/App";
 import rootReducer from "./reducers";
 
-const store = createStore(rootReducer);
+// Create a middleware to log the action information before executing it
+const logger =
+  ({ dispatch, getState }) =>
+  (next) =>
+  (action) => {
+    console.log("ACTION_TYPE", action);
+    next(action);
+  };
+
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
